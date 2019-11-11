834bc2e0b08648ac5f89c6ac4b120c066d0db8d1
docentSGS <docentsgs@gmail.com>
2019-11-11 20:18:26 +0200

    add behavior to validate forms

---
+++ main.js
@@ -0,0 +1,132 @@
+window.onload = formsValidate;
+
+let box1 = document.getElementById('box1');
+let box2 = document.getElementById('box2');
+const emailPattern = /^([a-z0-9_-]{1,15}\.){0,3}[a-z0-9_-]{1,15}@[a-z0-9_-]{1,15}\.[a-z]{2,3}$/;
+const passPattern = /^\S{8,}$/;
+const alphaNumericName = /^[0-9a-zA-Z]+$/;
+
+function formsValidate(){
+  let mail = document.querySelector('.email');
+  let password = document.querySelector('.password');
+  let signUp = document.getElementById('signUpBtn');
+  let userName = document.querySelector('.userName');
+  let house = document.querySelector('.select');
+  let comment = document.getElementById('aboutUser');
+  let saveData = document.getElementById('saveBtn');
+
+  if (mail) {
+    mail.addEventListener('blur', validateEmail);
+  }
+
+  if (password) {
+    password.addEventListener('blur', validatePassword);
+  }
+
+  if (signUp) {
+    signUp.addEventListener('click', function(e) {
+      e.preventDefault();
+      sendInfo();
+    });
+  }
+
+  if (userName) {
+    userName.addEventListener('blur', validateName);
+  }
+
+  if (house) {
+    house.addEventListener('blur', houseSelect);
+  }
+
+  if (comment) {
+    comment.addEventListener('blur', validateComment);
+  }
+
+  if (saveData) {
+    saveData.addEventListener('click', function(e) {
+      e.preventDefault();
+      submitData();
+    })
+  }
+
+  function validateEmail() {
+    if(mail.value.match(emailPattern)){
+      mail.classList.remove('input_error');
+    } else {
+      mail.classList.add('input_error');
+    }
+    mail.addEventListener('input', validateEmail);
+  }
+
+  function validatePassword() {
+    if (password.value.match(passPattern)) {
+      password.classList.remove('input_error');
+    } else {
+      password.classList.add('input_error');
+    }
+    password.addEventListener('input', validatePassword);
+  }
+
+  function sendInfo() {
+    if (mail.value.match(emailPattern)){
+      mail.classList.remove('input_error');
+      if (password.value.match(passPattern)){
+        password.classList.remove('input_error');
+        disp(box2);
+      }
+    }
+    if (!mailPatern.test(email.value)||!passPattern.test(password.value)) {
+      password.classList.add('input_error');
+      return;
+    }
+  }
+
+  function validateName() {
+    if (userName.value.match(alphaNumericName)) {
+      userName.classList.remove('input_error');
+    } else {
+      userName.classList.add('input_error');
+    }
+    userName.addEventListener('input', validateName);
+  }
+
+  function houseSelect() {
+    if (house.value=="Select") {
+      house.classList.add('input_error');
+      document.getElementById('house_note').innerHTML = '<br>Select your house from the list!';
+    } else {
+      house.classList.remove('input_error');
+      document.getElementById('house_note').innerHTML = '';
+    }
+  }
+
+  function validateComment() {
+    if (comment.value == '') {
+      comment.classList.add('input_error');
+      document.getElementById('comment_note').innerHTML = '<br>Please provide details!';
+    } else {
+      comment.classList.remove('input_error');
+      document.getElementById('comment_note').innerHTML = '';
+    }
+  }
+
+  function submitData() {
+    if (!alphaNumericName.test(userName.value)
+    ||house.value=='Select'||comment.value == '') {
+      return;
+    } else {
+      alert("Congratulate!");
+      document.querySelector('.formWithValidation2').submit();
+    }
+  }
+
+}
+
+function disp(box2) {
+  if (box2.style.display == 'none') {
+    box2.style.display = 'block';
+    box1.style.display = 'none';
+  } else {
+    box2.style.display = 'none';
+  }
+}
